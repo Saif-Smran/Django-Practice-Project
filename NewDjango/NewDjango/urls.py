81047acab5054c81afcc76549c1fa7d1d3a652df
Saif-Smran <smrangb@gmail.com>
2025-10-29 20:33:01 +0600

    Added Tailwind and Basic Admin page

--- NewDjango/NewDjango/urls.py
+++ NewDjango/NewDjango/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('tapp/', include('Tapp.urls')),
+
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
